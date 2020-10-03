360bfbaca2ebcb9b88aeaa95244dec02fe0dbb21
olkanalazek <72313610+olkanalazek@users.noreply.github.com>
2020-10-03 21:20:50 +0200

    dodaje choinka.html

--- choinka.js
+++ choinka.js
@@ -1,6 +1,9 @@
 // choinka
 
 const czastka = (y, padding = 0) => {
+    let bufor = [];
+    let nl = (typeof window === 'undefined') ? "\n" : "<br>";
+
     for (let loop = 0; loop < y; loop++) {
         let ileSpacji = loop + 1;
         let ileGwiazdek = y * 2 - 2 * loop - 1;
@@ -8,16 +11,22 @@ const czastka = (y, padding = 0) => {
         let spacje = ' '.padStart(ileSpacji, ' ');
         let gwiazdki = '*'.padStart(ileGwiazdek, '*');
 
-        console.log(' '.padStart(padding, ' '), spacje, gwiazdki);
+        bufor.push([ ' '.padStart(padding, ' '), spacje, gwiazdki ].join(''));
     }
+
+    return bufor.join(nl) + nl;
 };
 
 const choinka = (ile, krotnosc = 3) => {
+    let bufor = '';
+
     for (let loop = 1; loop <= ile; loop++) {
         let wysokosc = (ile - loop + 1) * krotnosc - 1;
 
-        czastka(wysokosc, loop * krotnosc);
+        bufor += czastka(wysokosc, loop * krotnosc);
     }
+
+    return bufor;
 };
 
-choinka(4, 5);
+console.log(choinka(4, 5));
