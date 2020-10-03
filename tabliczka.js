f4a64480290e5b0548280e2d9b88f0123f79c2be
olkanalazek <72313610+olkanalazek@users.noreply.github.com>
2020-10-03 19:35:41 +0200

    dodaje tabliczke 10 x 10

---
+++ tabliczka.js
@@ -0,0 +1,17 @@
+// tabliczka
+
+let x, y;
+let bufor = '';
+
+const pomnoz = (a, b) => {
+    return String(a * b).padStart(4);
+};
+
+for (y = 1; y <= 10; y++) {
+    for (x = 1; x <= 10; x++) {
+        bufor += ' ' + pomnoz(x, y);
+    }
+
+    console.log(bufor);
+    bufor = '';
+}
