028d0b6f53b3ed97a70f9c06e785394718f008bd
olkanalazek <72313610+olkanalazek@users.noreply.github.com>
2020-10-05 21:27:32 +0200

    dodaje losowe karty

--- memory/gra.js
+++ memory/gra.js
@@ -1,8 +1,32 @@
-const kartaClick = (karta) => {
-    karta.style.visibility = 'hidden';
+const zwrocKarte = (pies, tyl) => {
+    return `<div class="karta"><div pies="${pies}" bg="${(tyl + 1)}"></div></div>`;
 };
 
-document.querySelectorAll('.karta')
-    .forEach((karta) => {
-        karta.onclick = kartaClick.bind(null, karta);
-    });
+const nowaGra = () => {
+    const plansza = document.getElementById('plansza');
+
+    if (!plansza) {
+        return;
+    }
+
+    const kolejnosc = [...Array(15).keys(), ...Array(15).keys()]
+        .map((a) => ({ sort: Math.random(), value: a + 1 }))
+        .sort((a, b) => a.sort - b.sort)
+        .map((a) => a.value);
+
+    const karty = kolejnosc.map(x => zwrocKarte(x, x % 3));
+
+    plansza.innerHTML = karty.join('');
+
+    document.querySelectorAll('.karta')
+        .forEach((karta) => {
+            karta.onclick = kartaClick.bind(null, karta);
+        });
+};
+
+const kartaClick = (karta) => {
+    // karta.style.visibility = 'hidden';
+    karta.classList.add('zakryta');
+};
+
+nowaGra();
